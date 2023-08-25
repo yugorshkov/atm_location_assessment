bc5f443a02a1dc9c45be6bf01d26f982b030f1c8
Yuriy <gyuv28@gmail.com>
2023-08-25 14:06:03 +0300

    Правки в приложении и readme

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import folium
 import geopandas
 import streamlit as st
 from streamlit_folium import st_folium
@@ -8,19 +7,28 @@ from src.mc import create_minio_client
 def atms_app():
     st.set_page_config(page_title="Bankamatika", page_icon=":atm:")
     st.title(":bank: Bankamatika")
-    st.subheader("Оценка места размещения банкомата")
+    st.subheader("Оценка мест размещения банкоматов")
 
     col1, col2 = st.columns(2)
     with col1:
-        cities = {"Краснодар": "krasnodar", "Новороссийск": "novorossiysk", "Армавир": "armavir", "Ростов-на-Дону": "rostov"}
+        cities = {
+            "Краснодар": "krasnodar",
+            "Новороссийск": "novorossiysk",
+            "Армавир": "armavir",
+            "Ростов-на-Дону": "rostov",
+        }
         city = st.selectbox("Выберите город:", cities)
     with col2:
         working_hours = st.radio(
-            "Ожидаемое время работы:",
+            "Ожидаемый режим работы устройства:",
             ("круглосуточно", "до 23:00", "до 19:00"),
             index=1,
         )
-
+    st.info(
+        """Город разделён на шестиугольные зоны - гексы. Чем интенсивнее заливка, 
+        тем больше эта территория подходит для размещения банкоматов.""",
+        icon="ℹ️",
+    )
     minio_client = create_minio_client()
     url = minio_client.presigned_get_object(
         "atm-location-assessment", f"{cities[city]}-h3-atm-score.geojson"
@@ -40,29 +48,17 @@ def atms_app():
         k=5,
         legend_kwds={"caption": "Баллы", "scale": False},
         tooltip="location_score",
-        tooltip_kwds={
-            "aliases": ["Оценка территории: "]
-        },
-        popup=["placement", "population", "access_mode", "pois"],
+        tooltip_kwds={"aliases": ["Оценка территории: "]},
+        popup=["location_score"],
         popup_kwds={
             "aliases": [
-                "Объект размещения",
-                "Население",
-                "Время работы",
-                "Источники клиентопотока",
+                "Эффективность размещения банкомата в этой области по 100-бальной шкале:"
             ]
         },
-        tiles="OpenStreetMap",
         style_kwds={"fillOpacity": 0.7},
         name="h3_cells",
     )
-    st.info(
-        """Территория города разделена на небольшие шестиугольки (гексы).
-            Чем интенсивнее заливка гекса, тем больше эта территория подходит 
-            для размещения банкомата.""", 
-            icon="ℹ️"
-    )
-    st_data = st_folium(m, width=725, zoom=11)
+    st_folium(m, width=725, zoom=11)
 
 
 if __name__ == "__main__":
